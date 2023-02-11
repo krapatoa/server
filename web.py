b32e2438b70481591eb7b512e506cb8be0c536bf
lucatk <mail@lucakillmaier.com>
2023-02-11 21:47:23 +0000

    fix: image connection only possible once

--- web.py
+++ web.py
@@ -3,8 +3,9 @@ import cv2
 
 app = Flask(__name__)
 
+def camera():
+    global frame
 
-def get_frame():
     camera_port = 0
     camera = cv2.VideoCapture(camera_port)
 
@@ -12,14 +13,16 @@ def get_frame():
         retval, im = camera.read()
         imgencode = cv2.imencode('.jpg', im)[1]
         frame = imgencode.tostring()
+
+def frame_response():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 @app.route('/video')
 def video():
-    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(frame_response(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    camera()
