9c663a1858ec9050a787a558f07bb9e11b18f186
lucatk <mail@lucakillmaier.com>
2023-02-11 22:18:55 +0000

    fix name shadowing

--- web.py
+++ web.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 frame: Optional[bytes] = None
 
 
-def camera():
+def start_camera():
+    print("Camera")
     camera_port = 0
     camera = cv2.VideoCapture(camera_port)
     while True:
         global frame
         retval, im = camera.read()
+        print("read")
         imgencode = cv2.imencode('.jpg', im)[1]
         frame = imgencode.tostring()
 
@@ -34,4 +36,4 @@ def video():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
-    camera()
+    start_camera()
